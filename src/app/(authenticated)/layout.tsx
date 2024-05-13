92ecf2101d5b885902966c6abcd62f142a4add2a
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-13 14:22:23 -0300

    add autenticated layout

---
+++ src/app/(authenticated)/layout.tsx
@@ -0,0 +1,23 @@
+import { Header } from '@/components/Header'
+import { Sidebar } from '@/components/Sidebar'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'CoinKeeper',
+}
+
+export default function AuthenticatedLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <div className="grid min-h-screen w-full grid-cols-[200px_calc(100%-200px)]">
+      <Sidebar />
+      <div className="bg-blue-900 grid grid-rows-[5rem_calc(100vh-5rem)]">
+        <Header />
+        <section className="overflow-auto">{children}</section>
+      </div>
+    </div>
+  )
+}
