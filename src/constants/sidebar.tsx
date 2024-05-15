6b1962cc56145a073375f832ba9dec6d87152afe
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-15 13:57:46 -0300

    add sidebar constants

---
+++ src/constants/sidebar.tsx
@@ -0,0 +1,12 @@
+import { LuUsers, MdOutlineDashboard } from '@/assets/icons'
+
+export const SIDEBAR_ITEMS = {
+  Dashboard: {
+    path: '/dashboard',
+    icon: <MdOutlineDashboard size={20} />,
+  },
+  Usuários: {
+    path: '/users',
+    icon: <LuUsers size={20} />,
+  },
+}
