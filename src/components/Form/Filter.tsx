c59c2f00db6753ed531f2fb99ef2352b57dc1377
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-16 14:19:07 -0300

    add filter component

---
+++ src/components/Form/Filter.tsx
@@ -0,0 +1,24 @@
+import { tv } from 'tailwind-variants'
+import { Button } from './Button'
+
+const filter = tv({
+  slots: {
+    base: 'flex items-center gap-3',
+    select: 'border border-zinc-300 p-2 rounded-md',
+    input: 'border border-zinc-300 p-[6px] rounded-md',
+  },
+})
+
+const { base, select, input } = filter()
+
+export const Filter = () => {
+  return (
+    <div className={base()}>
+      <select className={select()}>
+        <option value="xuxu">Nome</option>
+      </select>
+      <input placeholder="Buscar" className={input()} />
+      <Button>Filtrar</Button>
+    </div>
+  )
+}
