3e606644934ef49dec1bcb3496e764418456d86a
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-16 14:18:24 -0300

    change logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 export const Logo = () => {
   return (
-    <span className="text-3xl mt-5">
+    <span className="text-4xl mt-5">
       <strong className="text-primary font-medium">Coin</strong>
       <strong className="font-medium">Keeper</strong>
     </span>
