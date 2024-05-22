9469f4003dcd81c42905298fb0e909ab397d9de4
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-22 13:37:27 -0300

    add env

---
+++ src/env.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error('Invalid envrioment variables.')
+
+  throw new Error('Invalid enviroment variables.')
+}
+
+export const env = parsedEnv.data
