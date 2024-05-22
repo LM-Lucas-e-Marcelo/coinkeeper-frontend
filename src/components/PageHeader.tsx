dc0299ba4f505444ddd048ca997a6705a3bea916
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-22 13:38:24 -0300

    remove margin from header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -2,7 +2,7 @@ import React, { ComponentProps } from 'react'
 import { tv } from 'tailwind-variants'
 
 const pageHeader = tv({
-  base: 'my-2 flex items-center justify-between bg-white w-full py-3 px-6',
+  base: 'mt-2 mb-8 flex items-center justify-between bg-white w-full',
 })
 
 export const PageHeader = ({ children }: ComponentProps<'div'>) => {
