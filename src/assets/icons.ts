3e015c270d9ced08fd6626a0d41294691cadbf84
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-24 15:05:35 -0300

    add new icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -1,3 +1,4 @@
+export { CgDanger } from 'react-icons/cg'
 export { MdOutlineDashboard, MdLogout } from 'react-icons/md'
 export { LuUsers, LuFilter } from 'react-icons/lu'
 export { IoIosSearch, IoIosNotificationsOutline } from 'react-icons/io'
