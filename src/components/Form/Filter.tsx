dcad90441393de22eb22447347cea5b3ca7ecb48
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-27 14:00:52 -0300

    add filter button

--- src/components/Form/Filter.tsx
+++ src/components/Form/Filter.tsx
@@ -1,5 +1,8 @@
+'use client'
 import { tv } from 'tailwind-variants'
 import { Button } from './button'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useUrlParams } from '@/hooks/useParams'
 
 const filter = tv({
   slots: {
@@ -11,14 +14,48 @@ const filter = tv({
 
 const { base, select, input } = filter()
 
-export const Filter = () => {
+interface FilterProps {
+  options: Array<{
+    name: string
+    value: string
+  }>
+}
+
+interface IForm {
+  per: string
+  content: string
+}
+
+export const Filter = ({ options }: FilterProps) => {
+  const { addParams, removeParams } = useUrlParams()
+  const { register, handleSubmit } = useForm<IForm>()
+
+  const onSubmit: SubmitHandler<IForm> = ({ content, per }): void => {
+    if (!content) {
+      return removeParams(['per', 'content'])
+    }
+
+    addParams({
+      per,
+      content,
+    })
+  }
+
   return (
-    <div className={base()}>
-      <select className={select()}>
-        <option value="xuxu">Nome</option>
+    <form className={base()} onSubmit={handleSubmit(onSubmit)}>
+      <select className={select()} {...register('per')}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.name}
+          </option>
+        ))}
       </select>
-      <input placeholder="Buscar" className={input()} />
+      <input
+        placeholder="Buscar"
+        className={input()}
+        {...register('content')}
+      />
       <Button>Filtrar</Button>
-    </div>
+    </form>
   )
 }
