c52b822dad039a6a54c7628de5cb27cb2f0edc2f
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-27 14:01:01 -0300

    add table loading

---
+++ src/components/loadings/table-loading.tsx
@@ -0,0 +1,44 @@
+import { Table } from '../table'
+
+const numberOfRows = 10
+
+const rows = Array.from({ length: numberOfRows })
+
+export const TableLoading = () => {
+  return (
+    <Table.Root>
+      <Table.Head>
+        <Table.Row>
+          <Table.Cell>Nome</Table.Cell>
+          <Table.Cell>Grupo</Table.Cell>
+          <Table.Cell>Usuário</Table.Cell>
+          <Table.Cell>Ações</Table.Cell>
+        </Table.Row>
+      </Table.Head>
+      <Table.Body>
+        {rows.map((_, index) => (
+          <Table.Row key={index}>
+            <Table.Cell>
+              {
+                <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-400 w-[90%] animate-pulse" />
+              }
+            </Table.Cell>
+            <Table.Cell>
+              {
+                <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-400 w-[80%] animate-pulse" />
+              }
+            </Table.Cell>
+            <Table.Cell>
+              {
+                <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-400 w-[90%] animate-pulse" />
+              }
+            </Table.Cell>
+            <Table.Cell>
+              <div className="h-8 bg-gray-200 rounded-md dark:bg-gray-400 w-[60%] animate-pulse" />
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table.Root>
+  )
+}
