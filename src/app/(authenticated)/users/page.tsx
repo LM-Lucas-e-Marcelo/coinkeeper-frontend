2ef096bf2a86fb434dbd5c0a4b539ba11cdabb6a
Lucas Sagás <lucas.sagas@rdstation.com>
2024-05-27 14:00:23 -0300

    add falback loading

--- src/app/(authenticated)/users/page.tsx
+++ src/app/(authenticated)/users/page.tsx
@@ -7,12 +7,25 @@ import { UsersTable } from './users-table'
 import { CreateUserModal } from '@/components/modals/users/create-user-modal'
 import { Suspense } from 'react'
 import { ModalButon } from '@/components/modal-button'
+import { TableLoading } from '@/components/loadings/table-loading'
 
-export default function Users() {
+export interface UsersProps {
+  searchParams: {
+    per?: string
+    content?: string
+  }
+}
+
+export default async function Users({ searchParams }: UsersProps) {
+  const filterOptions = [
+    { name: 'Nome', value: 'name' },
+    { name: 'Usuário', value: 'username' },
+  ]
+
   return (
     <>
       <PageHeader>
-        <Filter />
+        <Filter options={filterOptions} />
         <ButtonGroup>
           <Button isIcon>
             <LuFilter size={20} />
@@ -22,8 +35,8 @@ export default function Users() {
           </ModalButon>
         </ButtonGroup>
       </PageHeader>
-      <Suspense fallback={<h1>Loading</h1>}>
-        <UsersTable />
+      <Suspense fallback={<TableLoading />}>
+        <UsersTable searchParams={searchParams} />
         <CreateUserModal />
       </Suspense>
     </>
