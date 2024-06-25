a180cd473f9f0c3c9a202e4bd026dd3265b58187
Lucas Sagas <lucas.sagas@rdstation.com>
2024-06-25 13:56:43 -0300

    add sigini page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,26 @@
+import { Logo } from '@/components/logo'
+import { SignInForm } from '@/sign-in-form'
+import { tv } from 'tailwind-variants'
+
+const home = tv({
+  slots: {
+    container: 'w-full flex',
+    signInSection:
+      'w-[400px] p-6 flex items-center justify-center flex-col h-screen border-r border-r-gray gap-6',
+    imageSection: 'flex-1 bg-primaryWithOpacity',
+  },
+})
+
+const { container, signInSection, imageSection } = home()
+
 export default function Home() {
-  return <h1>Home</h1>
+  return (
+    <div className={container()}>
+      <section className={signInSection()}>
+        <Logo />
+        <SignInForm />
+      </section>
+      <section className={imageSection()} />
+    </div>
+  )
 }
