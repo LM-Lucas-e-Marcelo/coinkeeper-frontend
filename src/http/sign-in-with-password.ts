e12933c37168f5775fd8147a6d2f4c123dac93c2
Lucas Sagas <lucas.sagas@rdstation.com>
2024-06-25 13:58:26 -0300

    add http action

---
+++ src/http/sign-in-with-password.ts
@@ -0,0 +1,26 @@
+import { api } from './api-client'
+
+interface SignInWithPasswordRequest {
+  username: string
+  password: string
+}
+
+export interface SignInWithPasswordResponse {
+  token: string
+}
+
+export async function signInWithPassword({
+  username,
+  password,
+}: SignInWithPasswordRequest) {
+  const result = api
+    .post('auth/sign-in', {
+      json: {
+        username,
+        password,
+      },
+    })
+    .json<SignInWithPasswordResponse>()
+
+  return result
+}
