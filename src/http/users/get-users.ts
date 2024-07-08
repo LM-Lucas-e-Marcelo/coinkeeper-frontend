141f5fe1ca5923cb1c561f1ad247a26337409c97
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-08 19:03:13 -0300

    add get users

---
+++ src/http/users/get-users.ts
@@ -0,0 +1,45 @@
+import { api } from '../api-client'
+
+export interface IUser {
+  id: number
+  name: string
+  username: string
+  role: {
+    id: number
+    name: string
+  }
+}
+
+export interface IUsers {
+  items: IUser[]
+  total: number
+  currentPage: number
+  lastPage: number
+  limit: number
+}
+
+export interface IGetUsers {
+  per?: string
+  content?: string
+}
+
+export async function getUsers({
+  content = '',
+  per = '',
+}: IGetUsers): Promise<{ users: IUsers }> {
+  const queryParams = new URLSearchParams()
+
+  if (content) {
+    queryParams.append(per, content)
+  }
+
+  const result = api.get(`users/search?${queryParams.toString()}`, {
+    next: {
+      tags: ['users'],
+    },
+  })
+
+  const users = await result.json<IUsers>()
+
+  return { users }
+}
