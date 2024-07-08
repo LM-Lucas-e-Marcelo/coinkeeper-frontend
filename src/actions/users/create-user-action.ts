4421e7240047cf780ccc0940ac72dc9ffad64ddf
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-08 18:59:50 -0300

    refactor create user

--- src/actions/users/create-user-action.ts
+++ src/actions/users/create-user-action.ts
@@ -1,29 +1,45 @@
 'use server'
 
-import { Status } from '@/constants/status'
-import { api } from '@/services/api'
-import { IActionResponse } from '@/types/action-response'
-import { CreateUserData } from '@/types/users/create-user'
+import { createUser } from '@/http/users/create-user'
+import { HTTPError } from 'ky'
 import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
 
-export async function createUser(
-  data: CreateUserData,
-): Promise<IActionResponse> {
-  const { Error, Success } = Status
+const createUserSchema = z.object({
+  name: z.string().min(1, { message: 'Nome é obrigatório' }),
+  username: z.string().min(1, { message: 'Usuário é obrigatório' }),
+  password: z.string().min(1, { message: 'Senha é obrigatório' }),
+})
+
+export async function createUserAction(data: FormData) {
+  const result = createUserSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { name, password, username } = result.data
+
   try {
-    await api('/users', {
-      method: 'POST',
-      body: JSON.stringify(data),
-    })
+    await createUser({ name, password, username })
     revalidateTag('users')
-    return {
-      status: Success,
-      message: 'Usuário cadastrado com sucesso!',
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
     }
-  } catch (error) {
+
+    console.error(err)
+
     return {
-      status: Error,
-      message: 'Error message',
+      success: false,
+      message: 'Unexpected error, try again in a few minutes',
+      errors: null,
     }
   }
+
+  return { success: true, message: 'Usuário criado com sucesso', errors: null }
 }
