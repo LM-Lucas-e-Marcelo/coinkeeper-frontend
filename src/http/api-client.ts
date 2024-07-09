f90d2577719281cadfb35008159d3f4b552694ef
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-09 18:49:07 -0300

    add cookies on api client

--- src/http/api-client.ts
+++ src/http/api-client.ts
@@ -1,5 +1,25 @@
+import { getCookie } from 'cookies-next'
+import { CookiesFn } from 'cookies-next/lib/types'
 import ky from 'ky'
 
 export const api = ky.create({
-  prefixUrl: 'http://localhost:5000',
+  prefixUrl: 'http://170.245.13.216:8080',
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        let cookieStore: CookiesFn | undefined
+        if (typeof window === 'undefined') {
+          const { cookies: serverCookies } = await import('next/headers')
+
+          cookieStore = serverCookies
+        }
+
+        const token = getCookie('token', { cookies: cookieStore })
+
+        if (token) {
+          request.headers.set('tokenaccess', token)
+        }
+      },
+    ],
+  },
 })
