f62fa7ba4aa41f89fffdc2031060c6295eae91f1
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-09 18:49:32 -0300

    add select

---
+++ src/components/Form/select.tsx
@@ -0,0 +1,39 @@
+import { ComponentProps } from 'react'
+import { tv } from 'tailwind-variants'
+
+const select = tv({
+  slots: {
+    selectStyles: 'flex-1 bg-secondary p-3 rounded-md border border-primary',
+    labelStyles: 'w-full text-sm flex flex-col text-gray-500',
+    errorStyles: 'text-xs text-red mt-1 ml-1',
+  },
+})
+
+const { selectStyles, labelStyles, errorStyles } = select()
+
+interface OptionProps {
+  label: string
+  value: string | number
+}
+
+interface SelectProps extends ComponentProps<'select'> {
+  label?: string
+  error?: string[]
+  options: OptionProps[]
+}
+
+export function Select({ label, error, options, ...rest }: SelectProps) {
+  return (
+    <label htmlFor={label} className={labelStyles()}>
+      {label}
+      <select className={selectStyles()} id={label} {...rest}>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+      {!!error && <span className={errorStyles()}>{error[0]}</span>}
+    </label>
+  )
+}
