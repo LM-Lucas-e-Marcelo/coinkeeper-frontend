4198e9d79f5b4f9a9a4d12b0534c40f7f3859d71
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-16 19:35:53 -0300

    add create-customer-modal

---
+++ src/components/modals/create-customer-modal.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { ButtonGroup } from '@/components/form/button-group'
+import { Modal } from './modal'
+import { Button } from '@/components/form/button'
+import { Input } from '@/components/form/input'
+import { toast } from 'react-toastify'
+import { useCallback } from 'react'
+import { useUrlParams } from '@/hooks/use-params'
+import { useFormState } from '@/hooks/use-form-state'
+import { createCustomerAction } from '@/actions/customers/create-customer-action'
+
+export const CreateCustomerModal = () => {
+  const { removeParams, params } = useUrlParams()
+  const isOpen = params.has('create_customer')
+
+  const handleCloseModal = useCallback(() => {
+    removeParams(['create_customer'])
+  }, [removeParams])
+
+  const onSuccess = (message: string | null) => {
+    toast(message, { type: 'success' })
+    handleCloseModal()
+  }
+  const onError = (message: string | null) => {
+    toast(message, { type: 'error' })
+  }
+
+  const [{ errors }, handleSubmit, isPending] = useFormState({
+    action: createCustomerAction,
+    onError,
+    onSuccess,
+  })
+
+  return (
+    <Modal.Root isOpen={isOpen} onClose={handleCloseModal}>
+      <Modal.Header>Cadastrar Cliente</Modal.Header>
+      <form onSubmit={handleSubmit}>
+        <Modal.Content>
+          <div className="flex gap-3 flex-col">
+            <Input label="Nome" name="name" error={errors?.name} />
+            <Input label="CPF" name="document" error={errors?.document} />
+            <div className="flex gap-3">
+              <Input label="Telefone" name="phone" error={errors?.phone} />
+              <Input
+                label="Whatsapp"
+                name="phoneWhatsapp"
+                error={errors?.phoneWhatsapp}
+              />
+            </div>
+            <Input label="Endereço" name="address" error={errors?.address} />
+            <Input
+              label="Email"
+              type="email"
+              name="email"
+              error={errors?.email}
+            />
+          </div>
+        </Modal.Content>
+        <Modal.Actions>
+          <ButtonGroup>
+            <Button type="button" onClick={handleCloseModal} variant="danger">
+              Fechar
+            </Button>
+
+            <Button type="submit" disabled={isPending}>
+              Cadastrar
+            </Button>
+          </ButtonGroup>
+        </Modal.Actions>
+      </form>
+    </Modal.Root>
+  )
+}
