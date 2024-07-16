5d82d1c9a1634ef665b857db4a92ebfda69ec6a8
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-16 19:33:11 -0300

    add transaction schema

---
+++ fake-api/src/schemas/transactions.ts
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker'
+
+import {
+  IParcel,
+  ITransactions,
+} from '../../../src/http/transactions/get-transactions'
+
+function createRandomParcel(): IParcel {
+  return {
+    id: 26,
+    dueDate: '2024-01-09',
+    observation: null,
+    parcel: 1,
+    value: 1000,
+    paymentDate: '2024/01/09',
+  }
+}
+
+const parcels = faker.helpers.multiple(createRandomParcel, { count: 10 })
+
+export function createRandomTransaction(): ITransactions {
+  return {
+    id: faker.number.int({ min: 1, max: 1000 }),
+    totalParcels: 10,
+    totalParcelsPaid: faker.number.int({ min: 1, max: 9 }),
+    differenceBetweenParcels: faker.number.int({ min: 1, max: 10000 }),
+    description: faker.lorem.words(),
+    value: faker.number.int({ min: 1, max: 10000 }),
+    paymentDate: null,
+    parcels,
+  }
+}
+
+export const transactionList: ITransactions[] = faker.helpers.multiple(
+  createRandomTransaction,
+  {
+    count: 20,
+  },
+)
