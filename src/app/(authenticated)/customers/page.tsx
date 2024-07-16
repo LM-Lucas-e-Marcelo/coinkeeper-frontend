59e370cfa31aa063f5fe99182e2bf8d85857edb8
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-16 19:34:25 -0300

    add customer page

---
+++ src/app/(authenticated)/customers/page.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@/components/form/button'
+import { ButtonGroup } from '@/components/form/button-group'
+import { Filter } from '@/components/form/filter'
+import { PageHeader } from '@/components/page-header'
+import { LuFilter } from '@/assets/icons'
+import { CustomersTable } from './customers-table'
+import { Suspense } from 'react'
+import { TableLoading } from '@/components/loadings/table-loading'
+import { ModalButton } from '@/components/modal-button'
+
+export interface CustomersProps {
+  searchParams: {
+    per?: string
+    content?: string
+  }
+}
+
+export default async function Customers({ searchParams }: CustomersProps) {
+  const filterOptions = [{ name: 'Nome', value: 'name' }]
+
+  return (
+    <>
+      <PageHeader>
+        <Filter options={filterOptions} />
+        <ButtonGroup>
+          <Button isIcon>
+            <LuFilter size={20} />
+          </Button>
+          <ModalButton params={{ create_customer: true }}>
+            <Button>Cadastrar</Button>
+          </ModalButton>
+        </ButtonGroup>
+      </PageHeader>
+      <Suspense fallback={<TableLoading />}>
+        <CustomersTable searchParams={searchParams} />
+      </Suspense>
+    </>
+  )
+}
