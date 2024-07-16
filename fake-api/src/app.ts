e5203c76c526cf32710a3c99b0f7e5c718ac4b07
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-16 19:32:11 -0300

    add new routes

--- fake-api/src/app.ts
+++ fake-api/src/app.ts
@@ -2,11 +2,15 @@ import { Elysia } from 'elysia'
 import { usersRoute } from './routes/users'
 import { rolesRoute } from './routes/roles'
 import { authRoute } from './routes/auth'
+import { customersRoute } from './routes/customers'
+import { transactionsRoute } from './routes/transactions'
 
 const app = new Elysia()
 
 app.use(usersRoute)
 app.use(rolesRoute)
 app.use(authRoute)
+app.use(customersRoute)
+app.use(transactionsRoute)
 
 export { app }
