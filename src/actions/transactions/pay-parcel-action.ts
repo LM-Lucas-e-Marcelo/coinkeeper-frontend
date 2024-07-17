ea5ab5fd644f7df99f3555966bde0efe0b3d0a39
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-17 19:04:48 -0300

    add parcel action

---
+++ src/actions/transactions/pay-parcel-action.ts
@@ -0,0 +1,46 @@
+'use server'
+
+import { payParcel } from '@/http/transactions/pay-parcel'
+import { HTTPError } from 'ky'
+import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
+
+const payParcelSchema = z.object({
+  paymentDate: z.string().min(1, { message: 'Data é obrigatório' }),
+  id: z.string(),
+  transactionId: z.string(),
+  observation: z.string().nullish(),
+})
+
+export async function payParcelAction(data: FormData) {
+  const result = payParcelSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const { id, transactionId, paymentDate, observation } = result.data
+
+  try {
+    await payParcel({ id, transactionId, paymentDate, observation })
+    revalidateTag('transactions')
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: 'Parcela paga com sucesso', errors: null }
+}
