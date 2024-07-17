f5003adda9640b8ae29e496c0c64cf433651294b
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-17 19:05:44 -0300

    add new icons

--- src/assets/icons.ts
+++ src/assets/icons.ts
@@ -2,5 +2,12 @@ export { CgDanger } from 'react-icons/cg'
 export { MdOutlineDashboard, MdLogout } from 'react-icons/md'
 export { LuUsers, LuFilter } from 'react-icons/lu'
 export { IoIosSearch, IoIosNotificationsOutline } from 'react-icons/io'
-export { FiEdit, FiTrash2, FiArrowRight, FiEye } from 'react-icons/fi'
+export {
+  FiEdit,
+  FiTrash2,
+  FiArrowRight,
+  FiEye,
+  FiCheck,
+  FiX,
+} from 'react-icons/fi'
 export { RiUserSettingsLine } from 'react-icons/ri'
