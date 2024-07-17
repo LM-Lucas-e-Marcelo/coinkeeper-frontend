c7733c393170cf3745797268d16ded661cf3fa64
Lucas Sagas <lucas.sagas@rdstation.com>
2024-07-17 20:01:29 -0300

    add create transaction action

---
+++ src/actions/transactions/create-transaction-action.ts
@@ -0,0 +1,74 @@
+'use server'
+
+import { createTransaction } from '@/http/transactions/create-transaction'
+import { HTTPError } from 'ky'
+import { revalidateTag } from 'next/cache'
+import { z } from 'zod'
+
+const createTransactionSchema = z.object({
+  customerId: z.string(),
+  firstDueDate: z
+    .string()
+    .min(1, { message: 'Vencimento da primeira parcela é obrigatório' }),
+  totalParcels: z.coerce
+    .number()
+    .min(1, { message: 'Quantidade de parcelas é obrigatório' }),
+  description: z.string().min(1, { message: 'Descrição é obrigatório' }),
+  value: z.coerce.number().min(1, { message: 'Informe um valor válido' }),
+  differenceBetweenParcels: z.coerce.number().nullish(),
+  paymentDate: z.string().nullish(),
+})
+
+export async function createTransactionAction(data: FormData) {
+  const result = createTransactionSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+
+    return { success: false, message: null, errors }
+  }
+
+  const {
+    customerId,
+    firstDueDate,
+    totalParcels,
+    description,
+    value,
+    differenceBetweenParcels,
+    paymentDate,
+  } = result.data
+
+  try {
+    await createTransaction({
+      customerId,
+      firstDueDate,
+      totalParcels,
+      description,
+      value,
+      differenceBetweenParcels,
+      paymentDate,
+    })
+
+    revalidateTag('transactions')
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Empréstimo cadastrado com sucesso',
+    errors: null,
+  }
+}
