330a4962300b0281351edc335249412235e90e6e
Lucas Sagas <lucas.sagas@rdstation.com>
2024-08-06 13:33:01 -0300

    add input file

--- src/components/Form/input.tsx
+++ src/components/Form/input.tsx
@@ -3,9 +3,18 @@ import { tv } from 'tailwind-variants'
 
 const input = tv({
   slots: {
-    inputStyles: 'flex-1 bg-secondary p-3 rounded-md border border-primary',
+    inputStyles:
+      'flex-1 bg-secondary p-3 rounded-md border border-primary file:bg-primaryWithOpacity file:border-0 file:text-primary file:rounded-md file:mr-2',
     labelStyles: 'w-full text-sm flex flex-col text-gray-500',
     errorStyles: 'text-xs text-red mt-1 ml-1',
+  },
+
+  variants: {
+    isFile: {
+      true: {
+        inputStyles: 'py-1 pl-1 file:py-2 file:px-4 cursor-pointer',
+      },
+    },
   },
 })
 
@@ -16,11 +25,16 @@ interface InputProps extends ComponentProps<'input'> {
   error?: string[]
 }
 
-export function Input({ label, error, ...rest }: InputProps) {
+export function Input({ label, error, type, ...rest }: InputProps) {
   return (
     <label htmlFor={label} className={labelStyles()}>
       {label}
-      <input className={inputStyles()} id={label} {...rest} />
+      <input
+        className={inputStyles({ isFile: type === 'file' })}
+        id={label}
+        type={type}
+        {...rest}
+      />
       {!!error && <span className={errorStyles()}>{error[0]}</span>}
     </label>
   )
