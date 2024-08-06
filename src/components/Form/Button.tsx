556fe43417a6f9e90d2d08cfaa86801ffc9fef3f
Lucas Sagas <lucas.sagas@rdstation.com>
2024-08-06 16:15:36 -0300

    add new variant on default button

--- src/components/Form/Button.tsx
+++ src/components/Form/Button.tsx
@@ -2,13 +2,15 @@ import { ComponentProps } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const button = tv({
-  base: 'p-[7px] px-4 bg-primary text-white rounded-md min-h-8 hover:enabled:opacity-85 disabled:bg-gray-400 disabled:cursor-not-allowed',
+  base: 'p-[7px] px-4 bg-primary text-white rounded-md min-h-8 hover:enabled:opacity-85 disabled:bg-gray-400 disabled:cursor-not-allowed border-primary border',
   variants: {
     isIcon: {
       true: 'p-2',
     },
     variant: {
       danger: 'bg-red',
+      secondary:
+        'bg-transparent text-primary hover:enabled:bg-primary hover:enabled:text-white hover:enabled:opacity-1',
     },
   },
 })
