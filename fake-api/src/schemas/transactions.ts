d2fca5967b5fa78f82b6d423cd36b6e7dea14f0a
Lucas Sagas <lucas.sagas@rdstation.com>
2024-08-06 16:13:13 -0300

    add new routes on fake-api

--- fake-api/src/schemas/transactions.ts
+++ fake-api/src/schemas/transactions.ts
@@ -12,7 +12,10 @@ function createRandomParcel(): IParcel {
     observation: faker.lorem.words(),
     parcel: faker.number.int({ min: 1, max: 10 }),
     value: 1000,
-    paymentDate: '2024/01/09',
+    paymentDate: null,
+    proofFile: new Blob(),
+    proofFileUrl:
+      'https://www.gov.br/servidor/pt-br/acesso-a-informacao/faq/sou-gov.br/comprovante-de-rendimentos/comprovantes-de-rendimentos-imagens/6-tela-b-5-1passo-downloadrendimentopdf.jpg/@@images/image',
   }
 }
 
@@ -28,6 +31,11 @@ export function createRandomTransaction(): ITransactions {
     value: faker.number.int({ min: 1, max: 10000 }),
     paymentDate: '2024/01/09',
     valuePaid: faker.number.int({ min: 1, max: 9000 }),
+    companyExpense: faker.number.int({ min: 1, max: 9000 }),
+    contractFile: new Blob(),
+    contractFileUrl:
+      'https://www.gov.br/servidor/pt-br/acesso-a-informacao/faq/sou-gov.br/comprovante-de-rendimentos/comprovantes-de-rendimentos-imagens/6-tela-b-5-1passo-downloadrendimentopdf.jpg/@@images/image',
+
     createdAt: '2024/01/09',
     parcels,
   }
