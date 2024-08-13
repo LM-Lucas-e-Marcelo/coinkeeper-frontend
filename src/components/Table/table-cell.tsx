6dd8193e272e66a24c72ef91fad0660dadf4079d
Lucas Sagas <lucas.sagas@rdstation.com>
2024-08-13 20:24:25 -0300

    fix table cell

--- src/components/Table/table-cell.tsx
+++ src/components/Table/table-cell.tsx
@@ -1,9 +1,48 @@
+'use client'
+import { useUrlParams } from '@/hooks/use-params'
 import { ReactNode } from 'react'
+import { IoIosArrowDown } from '@/assets/icons'
+import { tv } from 'tailwind-variants'
+
+const orderIcon = tv({
+  base: 'transition-all',
+  variants: {
+    isAsc: {
+      true: 'rotate-180 ',
+      false: 'rotate-0',
+    },
+  },
+})
 
 interface TableCellProps {
   children: ReactNode
+  sortBy?: string
 }
 
-export const TableCell = ({ children }: TableCellProps) => {
-  return <td className="py-2 last:w-[100px] ">{children}</td>
+export const TableCell = ({ children, sortBy }: TableCellProps) => {
+  const { addParams, params } = useUrlParams()
+
+  const sortDirection = params.get('sortDirection')
+
+  const handleOrderBy = () => {
+    addParams({
+      sortDirection: sortDirection === 'asc' ? 'desc' : 'asc',
+      sortBy: sortBy!,
+    })
+  }
+
+  return (
+    <td className="py-2 last:w-[100px]">
+      {sortBy ? (
+        <button onClick={handleOrderBy} className="flex items-center gap-2">
+          {children}
+          <IoIosArrowDown
+            className={orderIcon({ isAsc: sortDirection === 'asc' })}
+          />
+        </button>
+      ) : (
+        children
+      )}
+    </td>
+  )
 }
