2ecd19e4925d9b7ccda5cad06c05fc1c99f5134c
Lucas Sagas <lucas.sagas@rdstation.com>
2024-09-17 16:39:54 -0300

    wip

---
+++ src/app/(authenticated)/customers/[id]/parcels.tsx
@@ -0,0 +1,123 @@
+'use client'
+import { Button } from '@/components/form/button'
+import { ButtonGroup } from '@/components/form/button-group'
+import { ModalButton } from '@/components/modal-button'
+import { IParcel } from '@/http/transactions/get-transactions'
+import { formatCurrency } from '@/utils/format-currency'
+import { formatDate } from '@/utils/format-date'
+import { tv } from 'tailwind-variants'
+
+const parcels = tv({
+  slots: {
+    container:
+      'flex flex-col px-4 p-2 gap-2 mb-4 border-l-4 border-red bg-secondary rounded-md last:mb-0 w-[90%] ml-auto',
+    wrapper: 'flex justify-between gap-2',
+    parcelSection: 'flex flex-col gap-2',
+    statusSpan: 'flex items-center gap-2',
+    section: 'flex flex-col gap-6 justify-start',
+  },
+  variants: {
+    paid: {
+      true: {
+        container: 'border-green-500',
+      },
+    },
+  },
+})
+
+interface ParcelsProps {
+  parcels: IParcel[]
+  transactionId: number
+}
+
+const { container, wrapper, parcelSection, statusSpan, section } = parcels()
+
+export const Parcels = ({ parcels, transactionId }: ParcelsProps) => {
+  const handleSeeProofPayment = (url: string) => {
+    window.open(url, '_blank')
+  }
+
+  return (
+    <>
+      {parcels?.map((parcel) => {
+        const currentParcelDate = new Date(parcel.dueDate)
+        const adjustDate = new Date(
+          currentParcelDate.setDate(currentParcelDate.getDate() + 1),
+        ).toISOString()
+
+        return (
+          <div
+            key={parcel.id}
+            className={container({ paid: parcel.paymentDate !== null })}
+          >
+            <div className={wrapper()}>
+              <section className={parcelSection()}>
+                <span className={statusSpan()}>
+                  <strong>Parcela:</strong>
+                  <p>{parcel.parcel}</p>
+                </span>
+                <span className={statusSpan()}>
+                  <strong>Valor:</strong>
+                  <p>{formatCurrency(parcel.value)}</p>
+                </span>
+              </section>
+              <section>
+                <span>
+                  <strong>Vencimento</strong>
+                  <p>{formatDate(adjustDate)}</p>
+                </span>
+              </section>
+              <section className={section()}>
+                <span>
+                  <strong>Pago em</strong>
+                  <p>{formatDate(parcel.paymentDate) ?? '-'}</p>
+                </span>
+              </section>
+            </div>
+            <span>
+              <strong>Observação</strong>
+              <p>{parcel.observation || '-'}</p>
+            </span>
+            <ButtonGroup align="end">
+              {parcel.proofFileUrl && (
+                <Button
+                  onClick={() => handleSeeProofPayment(parcel.proofFileUrl)}
+                  variant="secondary"
+                >
+                  Ver comprovante
+                </Button>
+              )}
+              {parcel.paymentDate !== null ? (
+                <Button type="button" variant="danger">
+                  <ModalButton
+                    params={{
+                      rollback_parcel: true,
+                      parcel: parcel.id,
+                      parcel_number: parcel.parcel,
+                      transaction_id: transactionId,
+                    }}
+                  >
+                    Cancelar pagamento
+                  </ModalButton>
+                </Button>
+              ) : (
+                <Button type="button">
+                  <ModalButton
+                    params={{
+                      pay_parcel: true,
+                      parcel: parcel.id,
+                      parcel_number: parcel.parcel,
+                      transaction_id: transactionId,
+                    }}
+                  >
+                    Pagar parcela
+                  </ModalButton>
+                </Button>
+              )}
+            </ButtonGroup>
+          </div>
+        )
+      })}
+    </>
+  )
+}
--- src/app/(authenticated)/customers/[id]/transaction-card.tsx
+++ src/app/(authenticated)/customers/[id]/transaction-card.tsx
@@ -1,7 +1,6 @@
 import { ModalButton } from '@/components/modal-button'
 import { tv } from 'tailwind-variants'
 import {
-  FiEye,
   FiTrash2,
   LiaFileContractSolid,
   FiCheck,
@@ -10,9 +9,9 @@ import {
 import { ITransactions } from '@/http/transactions/get-transactions'
 import { formatCurrency } from '@/utils/format-currency'
 import { formatDate } from '@/utils/format-date'
-import { ParcelsDetailsModal } from '@/components/modals/transactions/parcels-details-modal'
 import { PayOffTransactionModal } from '@/components/modals/transactions/pay-off-transaction-modal'
 import { FinancialLossModal } from '@/components/modals/transactions/financial-loss-modal'
+import { Parcels } from './parcels'
 
 const transactionCard = tv({
   slots: {
@@ -41,88 +40,88 @@ const { card, cardSection, secondCardSection, cardSpan, cardActions } =
 
 export function TransactionCard({ transaction }: TransactionCardProps) {
   return (
-    <div className={card({ paid: transaction.paymentDate !== null })}>
-      <div className={cardSection()}>
-        <span>
-          <strong>Descrição</strong>
-          <p>{transaction.description}</p>
-        </span>
-        <span>
-          <strong>Data</strong>
-          <p>{formatDate(transaction?.createdAt)}</p>
-        </span>
-        <span>
-          <strong>Valor da transação</strong>
-          <p className="text-red">{formatCurrency(transaction.value)}</p>
-        </span>
-        <span>
-          <strong>Valor pago</strong>
-          <p className="text-green-500">
-            {formatCurrency(transaction.valuePaid)}
-          </p>
-        </span>
-      </div>
-      <div className={secondCardSection()}>
-        <span className={cardSpan()}>
-          <strong>Parcelas</strong>
-          <p>
-            {transaction.totalParcelsPaid}/{transaction.totalParcels}
-          </p>
-        </span>
-        <span className={cardSpan()}>
-          <strong>Data da quitação</strong>
-          <p>{formatDate(transaction.paymentDate) || '-'}</p>
-        </span>
-        <span className={cardSpan()}>
-          <strong>Despesas</strong>
-          <p>{formatCurrency(transaction.companyExpense) || '-'}</p>
-        </span>
-        <span className={cardActions()}>
-          <ModalButton params={{ [`financial_loss_${transaction.id}`]: true }}>
-            <MdMoneyOff
-              size={24}
-              className="text-red"
-              title="Marcar como prejuízo"
-            />
-          </ModalButton>
-          <ModalButton
-            params={{ [`pay_off_transaction_${transaction.id}`]: true }}
-          >
-            <FiCheck
-              size={24}
-              className="text-green-500"
-              title="Efetuar quitação do empréstimo"
-            />
-          </ModalButton>
-          {transaction.contractFileUrl && (
-            <a href={transaction.contractFileUrl} target="_blank">
-              <LiaFileContractSolid
-                size={26}
-                className="text-orange"
-                title="Ver comprovante"
+    <div className="flex flex-col gap-3">
+      <div className={card({ paid: transaction.paymentDate !== null })}>
+        <div className={cardSection()}>
+          <span>
+            <strong>Descrição</strong>
+            <p>{transaction.description}</p>
+          </span>
+          <span>
+            <strong>Data</strong>
+            <p>{formatDate(transaction?.createdAt)}</p>
+          </span>
+          <span>
+            <strong>Valor da transação</strong>
+            <p className="text-red">{formatCurrency(transaction.value)}</p>
+          </span>
+          <span>
+            <strong>Valor pago</strong>
+            <p className="text-green-500">
+              {formatCurrency(transaction.valuePaid)}
+            </p>
+          </span>
+        </div>
+        <div className={secondCardSection()}>
+          <span className={cardSpan()}>
+            <strong>Parcelas</strong>
+            <p>
+              {transaction.totalParcelsPaid}/{transaction.totalParcels}
+            </p>
+          </span>
+          <span className={cardSpan()}>
+            <strong>Data da quitação</strong>
+            <p>{formatDate(transaction.paymentDate) || '-'}</p>
+          </span>
+          <span className={cardSpan()}>
+            <strong>Despesas</strong>
+            <p>{formatCurrency(transaction.companyExpense) || '-'}</p>
+          </span>
+          <span className={cardActions()}>
+            <ModalButton
+              params={{ [`financial_loss_${transaction.id}`]: true }}
+            >
+              <MdMoneyOff
+                size={24}
+                className="text-red"
+                title="Marcar como prejuízo"
               />
-            </a>
-          )}
-          <ModalButton params={{ [`parcels_details_${transaction.id}`]: true }}>
-            <FiEye size={24} className="text-primary" />
-          </ModalButton>
-          <ModalButton
-            params={{ delete_transaction: true, transaction: transaction.id }}
-          >
-            <FiTrash2
-              size={24}
-              className="text-red"
-              title="Excluir empréstimo"
-            />
-          </ModalButton>
-        </span>
-        <ParcelsDetailsModal
-          parcels={transaction.parcels}
-          transactionId={transaction.id}
-        />
-        <PayOffTransactionModal transaction={transaction} />
-        <FinancialLossModal transaction={transaction} />
+            </ModalButton>
+            <ModalButton
+              params={{ [`pay_off_transaction_${transaction.id}`]: true }}
+            >
+              <FiCheck
+                size={24}
+                className="text-green-500"
+                title="Efetuar quitação do empréstimo"
+              />
+            </ModalButton>
+            {transaction.contractFileUrl && (
+              <a href={transaction.contractFileUrl} target="_blank">
+                <LiaFileContractSolid
+                  size={26}
+                  className="text-orange"
+                  title="Ver comprovante"
+                />
+              </a>
+            )}
+            <ModalButton
+              params={{ delete_transaction: true, transaction: transaction.id }}
+            >
+              <FiTrash2
+                size={24}
+                className="text-red"
+                title="Excluir empréstimo"
+              />
+            </ModalButton>
+          </span>
+          <PayOffTransactionModal transaction={transaction} />
+          <FinancialLossModal transaction={transaction} />
+        </div>
       </div>
+      {!!transaction.parcels?.length && (
+        <Parcels parcels={transaction.parcels} transactionId={transaction.id} />
+      )}
     </div>
   )
 }
