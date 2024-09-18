fee6bc0dad9cba1a196ea5b8c531df5d259477f6
Lucas Sagas <lucas.sagas@rdstation.com>
2024-09-18 13:30:41 -0300

    convert colors to hex

--- src/utils/format-score-color.ts
+++ src/utils/format-score-color.ts
@@ -1,6 +1,6 @@
 export const formatScoreColors = (score: number) => {
-  if (score <= 250) return 'red'
-  if (score <= 500) return 'orange'
-  if (score <= 750) return 'yellow'
-  if (score > 750) return 'green'
+  if (score <= 250) return '#FF0000'
+  if (score <= 500) return '#FFAC1C'
+  if (score <= 750) return '#FFFF00'
+  if (score > 750) return '#00FF00'
 }
